834c3c4ffd128147c4b6e8e6c11f635fe568fb6b
roger.jiang <roger.jiang@afusion.com>
2019-04-09 22:08:14 +0800

    add insert sort test case

--- Algorithm/Sorting/__tests__/insertSort.test.js
+++ Algorithm/Sorting/__tests__/insertSort.test.js
@@ -1,5 +1,11 @@
 const insertSort = require('../insertSort')
 
-test('should sort [3,2,1] become [1,2,3]', () => {
+test('should sort array in ascending order', () => {
 	expect(insertSort([3, 2, 1])).toEqual([1, 2, 3])
+	expect(insertSort([3, 1, 2])).toEqual([1, 2, 3])
+
+	expect(insertSort([3, -1, 2])).toEqual([-1, 2, 3])
+	expect(insertSort([0, -1, 2])).toEqual([-1, 0, 2])
+
+	expect(insertSort([4, -1, -1, 2])).toEqual([-1, -1, 2, 4])
 })
