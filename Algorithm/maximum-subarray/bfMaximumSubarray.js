06d1ef17cfd385d691e40e19f656ffb6afc3ea6a
Roger Jiang <dkra4741@gmail.com>
2019-05-06 10:16:46 +0800

    maximum subarray brute force

---
+++ Algorithm/maximum-subarray/bfMaximumSubarray.js
@@ -0,0 +1,31 @@
+/**
+ * Brute Force solution.
+ * Complexity: O(n^2)
+ *
+ * @param {Number[]} inputArray
+ * @return {Number[]}
+ */
+
+const bfMaximumSubarray = arr => {
+	let maxSubArr = []
+	let headIdx
+	let tailIdx
+	let maxSum = Number.NEGATIVE_INFINITY
+	let accumulator = 0
+	if (arr.length <= 1) return arr
+	for (let i = 0; i < arr.length; i++) {
+		accumulator = 0
+		for (let j = i; j < arr.length; j++) {
+			accumulator = accumulator + arr[j]
+			if (accumulator > maxSum) {
+				maxSum = accumulator
+				headIdx = i
+				tailIdx = j
+			}
+		}
+	}
+
+	return arr.slice(headIdx, tailIdx + 1)
+}
+
+module.exports = bfMaximumSubarray
