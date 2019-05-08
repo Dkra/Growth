008e9b8b2885459a1418b6138ac05a04bcf45942
Roger Jiang <dkra4741@gmail.com>
2019-05-08 18:01:58 +0800

    wip

--- Algorithm/maximum-subarray/bfMaximumSubarray.js
+++ Algorithm/maximum-subarray/bfMaximumSubarray.js
@@ -28,4 +28,28 @@ const bfMaximumSubarray = arr => {
 	return arr.slice(headIdx, tailIdx + 1)
 }
 
-module.exports = bfMaximumSubarray
+const findMaxCrossArr = (arr, head, mid, tail) => {
+	let leftMaxSum = Math.NEGATIVE_INFINITY
+	let rightMaxSum = Math.NEGATIVE_INFINITY
+	let headIdx = null
+	let tailIdx = null
+	let accumulator = 0
+	for (let i = mid; i >= head; i--) {
+		accumulator += i
+		if (accumulator >= leftMaxSum) {
+			leftMaxSum = accumulator
+		}
+	}
+
+	accumulator = 0
+	for (let i = mid + 1; i <= tail; i++) {
+		accumulator += i
+		if (accumulator >= rightMaxSum) {
+			rightMaxSum = accumulator
+		}
+	}
+}
+
+const dqMaximumSubarray = arr => {}
+
+module.exports = dqMaximumSubarray
