d2316621872c00f2f0f67844f1a5752ac3ae1c2d
roger.jiang <roger.jiang@afusion.com>
2019-05-14 20:41:59 +0800

    add jest test plugin

--- DataStructure/__tests__/Stack.test.js
+++ DataStructure/__tests__/Stack.test.js
@@ -1,29 +1,29 @@
-const Stack = require('../Stack')
+const Stack = require("../Stack")
 let stack = new Stack()
 
 beforeEach(() => {
-	stack = new Stack()
+  stack = new Stack()
 })
 
-test('stack.length & push fuction', () => {
-	expect(stack.length).toBe(0)
-	stack.push(1)
-	expect(stack.length).toBe(1)
+test("stack.length & push fuction", () => {
+  expect(stack.length).toBe(0)
+  stack.push(1)
+  expect(stack.length).toBe(1)
 })
 
-test('IsEmpty', () => {
-	expect(stack.isEmpty()).toBe(true)
+test("IsEmpty", () => {
+  expect(stack.isEmpty()).toBe(true)
 })
 
-test('peek', () => {
-	stack.push(1)
-	stack.push(2)
-	expect(stack.peek()).toBe(1)
+test("peek", () => {
+  stack.push(1)
+  stack.push(2)
+  expect(stack.peek()).toBe(1)
 })
 
-test('pop', () => {
-	expect(stack.pop()).toBe(null)
-	stack.push(1)
-	stack.push(2)
-	expect(stack.pop()).toBe(2)
+test("pop", () => {
+  expect(stack.pop()).toBe(null)
+  stack.push(1)
+  stack.push(2)
+  expect(stack.pop()).toBe(2)
 })
