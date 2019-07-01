005f9947f3c9a8b9fcb4fb1bc78252c73d923a5c
roger.jiang <roger.jiang@afusion.com>
2019-07-01 18:47:28 +0800

    rewrite leetcode 27

--- CodingChallenge/LeetCode/Array/27. Remove Element/removeElement.js
+++ CodingChallenge/LeetCode/Array/27. Remove Element/removeElement.js
@@ -1,6 +1,17 @@
 /*
-  http://www.cnblogs.com/grandyang/p/4606700.html
+  https://leetcode.com/problems/remove-element/
 */
+
+/**
+ * Runtime: 52 ms, faster than 90.47 % of JavaScript online submissions for Remove Element.
+ * Memory Usage: 33.7 MB, less than 98.61 % of JavaScript online submissions for Remove Element.
+ */
+
+/**
+ * Approach: Insert Pointer start from 0
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
 
 /**
  * @param {number[]} nums
@@ -9,12 +20,13 @@
  */
 
 const removeElement = (nums, val) => {
-  const restNumArr = nums.filter(num => num !== val)
-  restNumArr.forEach((num, idx) => {
-    nums[idx] = num
-  })
-  // return restNumArr.length
-  return nums.slice(0, restNumArr.length) // for test
+  let insertPtr = 0
+  let length = nums.length
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) length--
+    else nums[insertPtr++] = nums[i]
+  }
+  return length
 }
 
 module.exports = removeElement
--- CodingChallenge/LeetCode/Array/27. Remove Element/removeElement.test.js
+++ CodingChallenge/LeetCode/Array/27. Remove Element/removeElement.test.js
@@ -1,6 +1,6 @@
 const removeElement = require("./removeElement")
 
 test("should remove duplicate", () => {
-  expect(removeElement([1, 1, 1, 2, 2, 3], 1)).toEqual([2, 2, 3])
-  expect(removeElement([0, 0, 1, 1, 1, 1, 2, 3, 3], 1)).toEqual([0, 0, 2, 3, 3])
+  expect(removeElement([1, 1, 1, 2, 2, 3], 1)).toEqual(3)
+  expect(removeElement([0, 0, 1, 1, 1, 1, 2, 3, 3], 1)).toEqual(5)
 })
